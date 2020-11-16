bcaa50ed09676281ea730d44cdddbbe31c26c8d1
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2020-11-16 23:11:48 +0100

    Both motors work, plotter moves both ways.

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ ninja -C build
 Use ninja or IDE to build. Upload with `west flash` and after this initial upload GDB from vscode also sohould work.
 
 * I copied `zephyrproject/zephyr/boards/arm/stm32f746g_disco` to my project to be able to modify the `stm32f746g_disco.dts` file.
+
+
+# TMC 2130
+spreadCycle is one of chopper algorithms:
+* 4 phases : on, slow decay, fast decay, second slow decay
+* blanking time masks the spikes.
+* chopper frequency
--- src/main.cc
+++ src/main.cc
@@ -26,106 +26,36 @@
 #define MOTOR1_NSS_PIN DT_GPIO_PIN (MOTOR1_NSS_NODE, gpios)
 #define MOTOR1_NSS_FLAGS DT_GPIO_FLAGS (MOTOR1_NSS_NODE, gpios)
 
-// TMC2130 registers
-#define WRITE_FLAG (1 << 7) // write flag
-#define READ_FLAG (0 << 7)  // read flag
-#define REG_GCONF 0x00
-#define REG_GSTAT 0x01
-#define REG_IHOLD_IRUN 0x10
-#define REG_CHOPCONF 0x6C
-#define REG_COOLCONF 0x6D
-#define REG_DCCTRL 0x6E
-#define REG_DRVSTATUS 0x6F
+/*--------------------------------------------------------------------------*/
 
-// namespace container {
+#define MOTOR2_DIR_NODE DT_PATH (motor2_pins, dir)
+#define MOTOR2_DIR_LABEL DT_GPIO_LABEL (MOTOR2_DIR_NODE, gpios)
+#define MOTOR2_DIR_PIN DT_GPIO_PIN (MOTOR2_DIR_NODE, gpios)
+#define MOTOR2_DIR_FLAGS DT_GPIO_FLAGS (MOTOR2_DIR_NODE, gpios)
 
-// TMC2130Stepper *aa ()
-// {
+#define MOTOR2_STEP_NODE DT_PATH (motor2_pins, step)
+#define MOTOR2_STEP_LABEL DT_GPIO_LABEL (MOTOR2_STEP_NODE, gpios)
+#define MOTOR2_STEP_PIN DT_GPIO_PIN (MOTOR2_STEP_NODE, gpios)
+#define MOTOR2_STEP_FLAGS DT_GPIO_FLAGS (MOTOR2_STEP_NODE, gpios)
 
-//         /*--------------------------------------------------------------------------*/
+#define MOTOR2_ENABLE_NODE DT_PATH (motor2_pins, enable)
+#define MOTOR2_ENABLE_LABEL DT_GPIO_LABEL (MOTOR2_ENABLE_NODE, gpios)
+#define MOTOR2_ENABLE_PIN DT_GPIO_PIN (MOTOR2_ENABLE_NODE, gpios)
+#define MOTOR2_ENABLE_FLAGS DT_GPIO_FLAGS (MOTOR2_ENABLE_NODE, gpios)
 
-//         // gpio_pin_set (enable, MOTOR1_ENABLE_PIN, true);
-//         return &stepper;
-// }
-
-// } // namespace container
-
-// int writeCmd (const struct device *spi, const struct spi_config *spiCfg, uint8_t cmd, uint32_t value)
-// {
-//         value = sys_cpu_to_be32 (value);
-//         const struct spi_buf bufs[] = {{.buf = &cmd, .len = 1}, {.buf = &value, .len = 4}};
-//         const struct spi_buf_set bufSet = {.buffers = bufs, .count = 2};
-//         return spi_write (spi, spiCfg, &bufSet);
-// }
+#define MOTOR2_NSS_NODE DT_PATH (motor2_pins, nss)
+#define MOTOR2_NSS_LABEL DT_GPIO_LABEL (MOTOR2_NSS_NODE, gpios)
+#define MOTOR2_NSS_PIN DT_GPIO_PIN (MOTOR2_NSS_NODE, gpios)
+#define MOTOR2_NSS_FLAGS DT_GPIO_FLAGS (MOTOR2_NSS_NODE, gpios)
 
 /****************************************************************************/
 
+// #define MOTOR1 1
+#define MOTOR2 1
+
 void main (void)
 {
-        bool stepState = true;
-
-        const struct device *dir = device_get_binding (MOTOR1_DIR_LABEL);
-
-        if (dir == NULL) {
-                return;
-        }
-
-        int ret = gpio_pin_configure (dir, MOTOR1_DIR_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_DIR_FLAGS);
-
-        if (ret < 0) {
-                return;
-        }
-
-        /*--------------------------------------------------------------------------*/
-
-        const struct device *step = device_get_binding (MOTOR1_STEP_LABEL);
-
-        if (step == NULL) {
-                return;
-        }
-
-        ret = gpio_pin_configure (step, MOTOR1_STEP_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_STEP_FLAGS);
-
-        if (ret < 0) {
-                return;
-        }
-
-        /*--------------------------------------------------------------------------*/
-
-        const struct device *enable = device_get_binding (MOTOR1_ENABLE_LABEL);
-
-        if (enable == NULL) {
-                return;
-        }
-
-        ret = gpio_pin_configure (enable, MOTOR1_ENABLE_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_ENABLE_FLAGS);
-
-        if (ret < 0) {
-                return;
-        }
-
-        /*--------------------------------------------------------------------------*/
-
-        const struct device *nss = device_get_binding (MOTOR1_NSS_LABEL);
-
-        if (nss == NULL) {
-                return;
-        }
-
-        ret = gpio_pin_configure (nss, MOTOR1_NSS_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_NSS_FLAGS);
-
-        if (ret < 0) {
-                return;
-        }
-
-        /*--------------------------------------------------------------------------*/
-
-        gpio_pin_set (enable, MOTOR1_ENABLE_PIN, false);
-        gpio_pin_set (dir, MOTOR1_DIR_PIN, false);
-
-        /*--------------------------------------------------------------------------*/
-
-        struct spi_config spiCfg = {0};
+        int ret{};
         const struct device *spi = device_get_binding (DT_LABEL (DT_NODELABEL (spi2)));
 
         if (!spi) {
@@ -133,23 +63,201 @@ void main (void)
                 return;
         }
 
-        const struct spi_cs_control spiCs = {.gpio_dev = nss, .delay = 0, .gpio_pin = MOTOR1_NSS_PIN, .gpio_dt_flags = GPIO_ACTIVE_LOW};
+#ifdef MOTOR1
+        const struct device *dir1 = device_get_binding (MOTOR1_DIR_LABEL);
 
-        spiCfg.operation = SPI_WORD_SET (8) | SPI_MODE_CPOL | SPI_MODE_CPHA;
-        spiCfg.frequency = 500000U;
-        spiCfg.cs = &spiCs;
+        if (dir1 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (dir1, MOTOR1_DIR_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_DIR_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
 
         /*--------------------------------------------------------------------------*/
 
-        TMC2130Stepper driver (spi, &spiCfg);
-        driver.rms_current (600);
-        driver.stealthChop (1);
+        const struct device *step1 = device_get_binding (MOTOR1_STEP_LABEL);
 
-        gpio_pin_set (enable, MOTOR1_ENABLE_PIN, true);
+        if (step1 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (step1, MOTOR1_STEP_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_STEP_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        /*--------------------------------------------------------------------------*/
+
+        const struct device *enable1 = device_get_binding (MOTOR1_ENABLE_LABEL);
+
+        if (enable1 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (enable1, MOTOR1_ENABLE_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_ENABLE_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        /*--------------------------------------------------------------------------*/
+
+        const struct device *nss1 = device_get_binding (MOTOR1_NSS_LABEL);
+
+        if (nss1 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (nss1, MOTOR1_NSS_PIN, GPIO_OUTPUT_ACTIVE | MOTOR1_NSS_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        gpio_pin_set (enable1, MOTOR1_ENABLE_PIN, false);
+        gpio_pin_set (dir1, MOTOR1_DIR_PIN, false);
+
+        const struct spi_cs_control spiCs1 = {.gpio_dev = nss1, .delay = 0, .gpio_pin = MOTOR1_NSS_PIN, .gpio_dt_flags = GPIO_ACTIVE_LOW};
+        struct spi_config spiCfg1 = {0};
+
+        spiCfg1.operation = SPI_WORD_SET (8) | SPI_MODE_CPOL | SPI_MODE_CPHA;
+        spiCfg1.frequency = 500000U;
+        spiCfg1.cs = &spiCs1;
+
+        TMC2130Stepper driver1 (spi, &spiCfg1);
+
+        // driver1.rms_current (600);
+        // driver1.stealthChop (1);
+        // driver1.microsteps (8);
+        driver1.dedge (true);
+        driver1.push ();
+        driver1.toff (3);
+        driver1.tbl (1);
+        driver1.chm (true);
+        driver1.hysteresis_start (4);
+        driver1.hysteresis_end (-2);
+        driver1.rms_current (800); // mA
+        driver1.microsteps (8);
+        // driver1.diag1_stall (1);
+        // driver1.diag1_active_high (1);
+        // driver1.coolstep_min_speed (0xFFFFF); // 20bit max
+        // driver1.THIGH (0);
+        // driver1.semin (5);
+        // driver1.semax (2);
+        // driver1.sedn (0b01);
+        // driver1.sg_stall_value (0);
+
+        gpio_pin_set (enable1, MOTOR1_ENABLE_PIN, true);
+#endif
+
+        /****************************************************************************/
+
+#ifdef MOTOR2
+        const struct device *dir2 = device_get_binding (MOTOR2_DIR_LABEL);
+
+        if (dir2 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (dir2, MOTOR2_DIR_PIN, GPIO_OUTPUT_ACTIVE | MOTOR2_DIR_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        /*--------------------------------------------------------------------------*/
+
+        const struct device *step2 = device_get_binding (MOTOR2_STEP_LABEL);
+
+        if (step2 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (step2, MOTOR2_STEP_PIN, GPIO_OUTPUT_ACTIVE | MOTOR2_STEP_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        /*--------------------------------------------------------------------------*/
+
+        const struct device *enable2 = device_get_binding (MOTOR2_ENABLE_LABEL);
+
+        if (enable2 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (enable2, MOTOR2_ENABLE_PIN, GPIO_OUTPUT_ACTIVE | MOTOR2_ENABLE_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        /*--------------------------------------------------------------------------*/
+
+        const struct device *nss2 = device_get_binding (MOTOR2_NSS_LABEL);
+
+        if (nss2 == NULL) {
+                return;
+        }
+
+        ret = gpio_pin_configure (nss2, MOTOR2_NSS_PIN, GPIO_OUTPUT_ACTIVE | MOTOR2_NSS_FLAGS);
+
+        if (ret < 0) {
+                return;
+        }
+
+        gpio_pin_set (enable2, MOTOR2_ENABLE_PIN, false);
+        gpio_pin_set (dir2, MOTOR2_DIR_PIN, false);
+
+        const struct spi_cs_control spiCs2 = {.gpio_dev = nss2, .delay = 0, .gpio_pin = MOTOR2_NSS_PIN, .gpio_dt_flags = GPIO_ACTIVE_LOW};
+        struct spi_config spiCfg2 = {0};
+
+        spiCfg2.operation = SPI_WORD_SET (8) | SPI_MODE_CPOL | SPI_MODE_CPHA;
+        spiCfg2.frequency = 500000U;
+        spiCfg2.cs = &spiCs2;
+
+        TMC2130Stepper driver2 (spi, &spiCfg2);
+
+        // driver2.rms_current (600);
+        // driver2.stealthChop (1);
+        // driver2.microsteps (8);
+        driver2.dedge (true);
+        driver2.push ();
+        driver2.toff (3);
+        driver2.tbl (1);
+        driver2.chm (true);
+        driver2.hysteresis_start (4);
+        driver2.hysteresis_end (-2);
+        driver2.rms_current (800); // mA
+        driver2.microsteps (8);
+        // driver2.diag1_stall (1);
+        // driver2.diag1_active_high (1);
+        // driver2.coolstep_min_speed (0xFFFFF); // 20bit max
+        // driver2.THIGH (0);
+        // driver2.semin (5);
+        // driver2.semax (2);
+        // driver2.sedn (0b01);
+        // driver2.sg_stall_value (0);
+
+        gpio_pin_set (enable2, MOTOR2_ENABLE_PIN, true);
+#endif
+
+        bool stepState = true;
 
         while (1) {
-                gpio_pin_set (step, MOTOR1_STEP_PIN, (int)stepState);
+#ifdef MOTOR1
+                gpio_pin_set (step1, MOTOR1_STEP_PIN, (int)stepState);
+#endif
+
+#ifdef MOTOR2
+                gpio_pin_set (step2, MOTOR2_STEP_PIN, (int)stepState);
+#endif
                 stepState = !stepState;
-                k_msleep (1);
+                k_usleep (100);
         }
 }
