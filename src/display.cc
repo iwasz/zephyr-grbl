c3dd89177eac881b376bf6baec7aded337da5a29
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2021-09-28 15:37:50 +0200

    Basic menu on the OLED

--- src/display.cc
+++ src/display.cc
@@ -7,14 +7,22 @@
  ****************************************************************************/
 
 #include "display.h"
+#include "Machine.h"
+#include "cfb_font_oldschool.h"
 #include "grblState.h"
 #include <device.h>
 #include <devicetree.h>
+#include <display/cfb.h>
 #include <drivers/gpio.h>
 #include <logging/log.h>
 #include <zephyr.h>
 
 LOG_MODULE_REGISTER (display);
+
+namespace {
+void displayThread (void *, void *, void *);
+K_THREAD_DEFINE (disp, 2048, displayThread, NULL, NULL, NULL, 14, 0, 0);
+} // namespace
 
 namespace disp {
 
@@ -33,6 +41,8 @@ namespace {
 #define ENCODER_ENTER_PIN DT_GPIO_PIN (ENCODER_ENTER_NODE, gpios)
 #define ENCODER_ENTER_FLAGS DT_GPIO_FLAGS (ENCODER_ENTER_NODE, gpios)
         const struct device *enter;
+
+        const struct device *display;
 
         gpio_callback encoderCallback;
         k_timer encoderDebounceTimer;
@@ -83,19 +93,6 @@ namespace {
                 }
         }
 
-        void displayThread (void *, void *, void *)
-        {
-                while (true) {
-                        k_sleep (K_MSEC (40));
-
-                        if (encoderEnter) {
-                                encoderEnter = false;
-                                grbl::jog (grbl::JogDirection::yPositive);
-                        }
-                }
-        }
-
-        K_THREAD_DEFINE (disp, 2048, displayThread, NULL, NULL, NULL, 14, 0, 0);
 } // namespace
 
 void init ()
@@ -132,6 +129,110 @@ void init ()
         if (ret != 0) {
                 LOG_ERR ("Encoder init failed");
         }
+
+        /*--------------------------------------------------------------------------*/
+
+        uint16_t rows;
+        uint8_t ppt;
+        uint8_t font_width;
+        uint8_t font_height;
+
+        display = device_get_binding ("SSD1306");
+
+        if (display == NULL) {
+                LOG_ERR ("Device not found\n");
+                return;
+        }
+
+        if (display_set_pixel_format (display, PIXEL_FORMAT_MONO10) != 0) {
+                LOG_ERR ("Failed to set required pixel format\n");
+                return;
+        }
+
+        if (cfb_framebuffer_init (display)) {
+                LOG_ERR ("Framebuffer initialization failed!\n");
+                return;
+        }
+
+        cfb_framebuffer_clear (display, true);
+        display_blanking_off (display);
+
+        rows = cfb_get_display_parameter (display, CFB_DISPLAY_ROWS);
+        ppt = cfb_get_display_parameter (display, CFB_DISPLAY_PPT);
+
+        // for (int idx = 0; idx < 42; idx++) {
+        //         if (cfb_get_font_size (dev, idx, &font_width, &font_height)) {
+        //                 break;
+        //         }
+
+        //         // if (font_height == ppt) {
+        //         cfb_framebuffer_set_font (dev, idx);
+        //         printf ("font width %d, font height %d\n", font_width, font_height);
+        //         // }
+        // }
+
+        // printf ("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n", cfb_get_display_parameter (dev, CFB_DISPLAY_WIDTH),
+        //         cfb_get_display_parameter (dev, CFB_DISPLAY_HEIGH), ppt, rows, cfb_get_display_parameter (dev, CFB_DISPLAY_COLS));
+
+        cfb_framebuffer_set_font (disp::display, 0);
 }
 
 } // namespace disp
+
+/**
+ * UI related.
+ */
+namespace {
+
+auto menu = [] (int option) {
+        cfb_framebuffer_clear (disp::display, false);
+        uint16_t margin = 2;
+
+        cfb_print (disp::display, "1. Y+", margin, 0);
+        cfb_print (disp::display, "2. Y-", margin, 8);
+        cfb_print (disp::display, "3. X+", margin, 16);
+        cfb_print (disp::display, "4. X-", margin, 24);
+        cfb_print (disp::display, "5. Ala ma kota", margin, 32);
+
+        cfb_print (disp::display, ">", 2, option * 8);
+        cfb_framebuffer_finalize (disp::display);
+};
+
+enum class Event { none, left, right, enter };
+
+constexpr auto enter = [] (Event e) { return e == Event::enter; };
+constexpr auto left = [] (Event e) { return e == Event::left; };
+constexpr auto right = [] (Event e) { return e == Event::right; };
+
+using namespace ls;
+auto menuMachine = machine ( // Jog menu
+        state ("JOG_YP"_ST, entry ([] { menu (0); }), transition ("JOG_YN"_ST, enter)),
+        state ("JOG_YN"_ST, entry ([] { menu (1); }), transition ("JOG_XP"_ST, enter)),
+        state ("JOG_XP"_ST, entry ([] { menu (2); }), transition ("JOG_XN"_ST, enter)),
+        state ("JOG_XN"_ST, entry ([] { menu (3); }), transition ("JOG_YP"_ST, enter)));
+
+void displayThread (void *, void *, void *)
+{
+        using namespace disp;
+
+        // Enter into initial state, and execute its entry state (that displays the menu).
+        menuMachine.run (Event::none);
+
+        while (true) {
+                if (disp::encoderEnter) {
+                        disp::encoderEnter = false;
+                        // grbl::jog (grbl::JogDirection::yPositive);
+                        menuMachine.run (Event::enter);
+                }
+
+                if (disp::encoderDirection == EncoderDirection::left) {
+                        menuMachine.run (Event::left);
+                }
+                else if (disp::encoderDirection == EncoderDirection::right) {
+                        menuMachine.run (Event::right);
+                }
+
+                k_sleep (K_MSEC (40));
+        }
+}
+} // namespace
